e26ecbfbe2146cc0c3f5b13f9505b090b1c384a7
Alex Bogushenko <bogushenkoas@ukr.net>
2023-09-30 01:03:10 +0300

    2 commit

--- main.cpp
+++ main.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
+#include <algorithm>
+
 
 using namespace std;
 
 mutex mtx;
 
-void info(string name, int val)
+
+vector<pair<string, double>> result;
+
+void info(string name, double val)
 {
-    
-    int distance = 0;
-    while(distance < 100)
+    double distance = 0;
+    while (distance < 100)
     {
-       
-       this_thread :: sleep_for(chrono :: seconds(1));
-       mtx.lock();
-       distance += val;
-       cout << name << ": " << distance << endl;
-       mtx.unlock();
+        this_thread::sleep_for(chrono::seconds(1));
+        mtx.lock();
+        distance += val;
+        if(distance > 100)
+        {
+            distance = 100;
+        }
+        cout << name << ": " << distance << endl;
+        mtx.unlock();
     }
-   
+
+     mtx.lock();
+    result.push_back(make_pair(name, distance / val));
+     mtx.unlock();
 }
+
 
 
 
 int main()
 {
   string swimmer1 = "swimmer_1";
-  int time1 = 3;
+  double time1 = 3;
   string swimmer2 = "swimmer_2";
-  int time2 = 4;
+  double time2 = 7;
   string swimmer3 = "swimmer_3";
-  int time3 = 5;
+  double time3 = 5;
   string swimmer4 = "swimmer_4";
-  int time4 = 6;
+  double time4 = 8;
   string swimmer5 = "swimmer_5";
-  int time5 = 7;
+  double time5 = 4;
   string swimmer6 = "swimmer_6";
-  int time6 = 8;
+  double time6 = 6;
 
   thread t1 (info, swimmer1, time1);
   thread t2 (info, swimmer2, time2);
@@ -52,6 +64,13 @@ int main()
   t4.join();
   t5.join();
   t6.join();
+
+
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i].first << " took " << i + 1 << " place with a time of " << result[i].second << " seconds" << endl;
+    }
+
   
  return 0;
 }
